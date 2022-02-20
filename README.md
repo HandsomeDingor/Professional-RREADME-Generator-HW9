cde25b56b464f6a7420e1f473efdfc18445f0836
HandsomeDingor <jay807541931@gmail.com>
2022-02-20 06:06:29 -0500

    add license section adn check if no license

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Professional-RREADME-Generator-HW9
 
 [README guide](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license ===  'mit'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+  }else {return '' }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license ===  'mit'){
+    return '(https://opensource.org/licenses/MIT)';
+  }else {return '' }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license ===  'mit'){
+    return '';
+  }else {return ''; }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-## Table-Contents
+## [License](#table-of-contents)
+The application is covered under the following license:
+${renderLicenseSection(data.license)}
+${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+  
+# ${data.title}
+
+## Table of Contents
 
 * [Description](#description)
 * [Installation](#installation)
@@ -24,26 +43,25 @@ function generateMarkdown(data) {
 * [License](#License)
 * [Questions](#questions)
     
-## [Description](#table-contents)
+## [Description](#table-of-contents)
 ${data.description}
 
-## [Installation](#table-contents)
+## [Installation](#table-of-contents)
 ${data.installation}
 
-## [Usage](#table-contents)
+## [Usage](#table-of-contents)
 ${data.usage}
 
-## [Contributing](#table-contents)
+## [Contributing](#table-of-contents)
 ${data.contributing}
 
-## [Tests](#table-contents)
+## [Tests](#table-of-contents)
 ${data.test}
 
-## [Questions](#table-contents)
+
+## [Questions](#table-of-contents)
 Please contact me using the following links:
-
 [GitHub](https://github.com/${data.github})
-
 [Email: ${data.email}](mailto:${data.email})
 `;
 }
